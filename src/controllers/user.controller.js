f3744a45dbffb87f4671ccb32adb82350327b8c2
Prabhat-Deshmukh022 <prabhat.deshmukh22@gmail.com>
2024-04-07 17:23:48 +0530

    aggregation pipelines fully done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js";
 import {cloudinary_upload} from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessandRefreshTokens = async (userId) => {
     try {
@@ -267,4 +268,111 @@ const updateAvatar = asyncHandler( async (req,res) => {
     .json(user.avatar)
 } )
 
-export {loginUser, registerUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, updateUserInfo, updateAvatar}
+const getChannels = asyncHandler( async (req,res) => {
+
+    const username = req.params.username
+    if(!username){
+        console.log("Username not received!");
+    }
+
+    const channels = await User.aggregate([{
+        $match: {username: username}
+    },
+    {
+        $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "owner",
+            as: "subscribers"
+        }
+    },
+    {
+        $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+        }
+    },
+    {
+        $addFields: {
+            subscriberCount: {
+                $size: "$subscribers"
+            },
+            channelsSubscribedTo: {
+                $size: "$subscribedTo"
+            },
+            isSubscribed: {
+                $cond: {
+                    if: {$in: [req.user._id, "$subscribers.subscriber"]},
+                    then: true,
+                    else: false
+                }
+            }
+        }
+    },
+    {
+        $project: {
+            username: 1,
+            avatar: 1,
+            subscriberCount: 1,
+            channelsSubscribedTo: 1,
+            isSubscribed: 1
+        }
+    }])
+
+    if (!channels) {
+        console.log("Channels does not exist!");
+    }
+
+    return res
+    .status(200)
+    .json(channels[0])
+
+} )
+
+const watchHistory = asyncHandler( async (req,res) => {
+    const user = await User.aggregate([{
+        $match: {
+            _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+    },
+    {
+        $lookup: {
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner",
+                        pipeline: [{
+                            $project:{
+                                username: 1,
+                                avatar: 1
+                            }
+                        }]
+                    }
+                },
+                {
+                    $addFields:{
+                        owner: {
+                            $first: "$owner"
+                        }
+                    }
+                }
+            ]
+        }   
+    }])
+
+    return res
+    .status(201)
+    .json(user[0].watchHistory)
+
+} )
+
+export {loginUser, registerUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, updateUserInfo, updateAvatar, getChannels, watchHistory}
